added cmd line args; restored larger weight init, fixed logprob bug, added peephole connections. halfcheetah converged eventually (without peepholes). beginning work on replay memories

a3c.py
@@ -64,7 +64,35 @@ parser = argparse.ArgumentParser(description='Run a3c.')
 parser.add_argument('--render', dest='visualize_global', action='store_true',
                     help='render the global network applied to a test environment')
 
+parser.add_argument('--tag', dest='tag', action='store',
+                    default="",
+                    help='tag to label the summary data')
+
+parser.add_argument('--hidden', dest='hidden_sizes', action='append',
+                    default=[200],
+                    help='number of units in 1st fc hidden layer')
+
+parser.add_argument('--lstm', dest='lstm_sizes', action='append',
+                    default=[128],
+                    help='number of units in lstm layer')
+
+parser.add_argument('--lr', dest='initial_learning_rate', action='store',
+                    default=initial_learning_rate, type=float,
+                    help='initial learning rate')
+
+parser.add_argument('--max_time_step', dest='max_time_step', action='store',
+                    default=constants.MAX_TIME_STEP, type=int,
+                    help='max time step to run for')
+
+parser.add_argument('--gym_env', dest='gym_env', action='store',
+                    default=constants.GYM_ENV,
+                    help='gym environment to use')
+
+
 args = parser.parse_args()
+print "*********************************************************"
+print "Run configuration:", args
+print "*********************************************************"
 
 visualize_global = args.visualize_global
 
@@ -72,7 +100,7 @@ visualize_global = args.visualize_global
 ## network setup
 # global_network = GameACNetwork(ACTION_SIZE, device)
 
-env = gym.make(constants.GYM_ENV)
+env = gym.make(args.gym_env)
 
 print("Env observation space:", env.observation_space)
 
@@ -88,8 +116,8 @@ else:
 global_network = LowDimACNetwork(continuous_mode=constants.CONTINUOUS_MODE,
                                  action_size=action_size,
                                  input_size=input_size,
-                                 hidden_sizes=[64],
-                                 lstm_sizes=[64],
+                                 hidden_sizes=args.hidden_sizes, #[200],
+                                 lstm_sizes=args.lstm_sizes, #[128],
                                  network_name="global-net",
                                  device=device)
 
@@ -107,11 +135,11 @@ grad_applier = RMSPropApplier(learning_rate = learning_rate_input,
                               device = device)
 
 for i in range(PARALLEL_SIZE):
-  training_thread = A3CTrainingThread(i, global_network, initial_learning_rate,
+  training_thread = A3CTrainingThread(i, global_network, args.initial_learning_rate,
                                       learning_rate_input,
-                                      grad_applier, MAX_TIME_STEP,
+                                      grad_applier, args.max_time_step,
                                       device = device,
-                                      environment=gym.make(constants.GYM_ENV))
+                                      environment=gym.make(args.gym_env))
   training_threads.append(training_thread)
 
 
@@ -127,7 +155,16 @@ def setup_summaries(sess):
     ROOT_LOG_DIR = constants.LOG_FILE #os.getcwd() + "/tf-log/"
     TODAY_LOG_DIR = ROOT_LOG_DIR + "/" + datetime.now().date().isoformat()
 
-    LOG_DIR = TODAY_LOG_DIR + "/" + datetime.now().isoformat('_').replace(':', '.')
+    LOG_DIR = TODAY_LOG_DIR + "/" + datetime.now().time().replace(second=0, microsecond=0).isoformat()[0:-3].replace(':', '.')
+
+    LOG_DIR += " %s" % args.gym_env
+    LOG_DIR += " lr=%f" % args.initial_learning_rate
+    LOG_DIR += " hs=%s" % args.hidden_sizes
+    LOG_DIR += " lstms=%s " % args.lstm_sizes
+
+    if len(args.tag) > 0:
+        LOG_DIR += " -- %s" % args.tag
+
 
     score_input = tf.placeholder(tf.float32,name="score_input")
     score_input_avg = tf.placeholder(tf.float32,name="score_input_avg")
@@ -196,7 +233,17 @@ for v in vars:
 ###############################################################################
 # init or load checkpoint with saver
 saver = tf.train.Saver() # TODO: just save/restore global net params
-checkpoint = tf.train.get_checkpoint_state(CHECKPOINT_DIR)
+
+checkpoint_name = "checkpoint"
+checkpoint_name += " %s" % args.gym_env
+checkpoint_name += " hs=%s" % args.hidden_sizes
+checkpoint_name += " lstms=%s " % args.lstm_sizes
+
+if len(args.tag) > 0:
+    checkpoint_name += " -- %s" % args.tag
+
+
+checkpoint = tf.train.get_checkpoint_state(CHECKPOINT_DIR) 
 if checkpoint and checkpoint.model_checkpoint_path:
   saver.restore(sess, checkpoint.model_checkpoint_path)
   print "checkpoint loaded:", checkpoint.model_checkpoint_path
@@ -208,25 +255,44 @@ else:
   print "Could not find old checkpoint"
 
 
+def save_checkpoint(name='checkpoint'):
+
+    if not os.path.exists(CHECKPOINT_DIR):
+      os.mkdir(CHECKPOINT_DIR)
+
+    saver.save(sess, CHECKPOINT_DIR + '/' + name, global_step=global_t)
+
+def should_save_checkpoint(global_t, checkpoint_every, checkpoint_count):
+    return (math.floor(global_t/checkpoint_every) > checkpoint_count)
+
+###################################################################
+
 def train_function(parallel_index):
-  global global_t
-  global stop_requested
-  
-  training_thread = training_threads[parallel_index]
+    global global_t
+    global stop_requested
 
-  while True:
-    if stop_requested:
-      break
-    if global_t > MAX_TIME_STEP:
-      break
+    training_thread = training_threads[parallel_index]
 
-    diff_global_t = training_thread.process(sess, global_t, summary_writer,
-                                            record_score_fn)
-                                            #score_summary_op, score_input)
-    global_t += diff_global_t
+    checkpoint_count = 0
+
+    while True:
+        if stop_requested:
+            break
+        if global_t > args.max_time_step:
+            break
+
+        diff_global_t = training_thread.process(sess, global_t, summary_writer,
+                                                record_score_fn)
+        # score_summary_op, score_input)
+        global_t += diff_global_t
 
 
-    
+        # global checkpoint saving
+        if parallel_index == 0 and should_save_checkpoint(global_t, 1000000, checkpoint_count):
+            checkpoint_count += 1
+            print "Saving checkpoint %d at t=%d" % (checkpoint_count, global_t)
+            save_checkpoint(name=checkpoint_name)
+
     
 def signal_handler(signal, frame):
   global stop_requested
@@ -248,13 +314,13 @@ def vis_network_thread(network):
 
     global stop_requested
     global global_t
-    #from a3c_training_thread import choose_action
-    env = gym.make(constants.GYM_ENV)
 
+    # init env & states
+    env = gym.make(args.gym_env)
     game_state = gym_game_state.GymGameState(0, display=True, no_op_max=0, env=env)  # resets env already
-    #env.render()
-    print "vis thread started"
     lstm_state = network.lstm_initial_state_value
+
+    print "vis thread started"
 
     episode_reward = 0
     while True:
@@ -263,7 +329,7 @@ def vis_network_thread(network):
         #print "vis thread step"
         if stop_requested:
             break
-        if global_t > MAX_TIME_STEP:
+        if global_t > args.max_time_step:
             break
 
         # print "lstm state:", lstm_state
@@ -286,12 +352,20 @@ def vis_network_thread(network):
         episode_reward += game_state.reward
         if game_state.terminal:
 
-            # todo: avg episode rewards and display those
+
             print "EPISODE REWARD: ", episode_reward
             episode_reward = 0
+
+            #  TODO: try trapping exceptions on this thread...see if can avoid bad state that way
+
+
+            # env.render(close=True) # does close window but doesnt help reset after exceptions...
+            # env.close() # TODO: validate that this is stable/efficient
+
+            # reinit env & states
+            # env = gym.make(args.gym_env)
             game_state = gym_game_state.GymGameState(0, display=True, no_op_max=0, env=env)
             lstm_state = network.lstm_initial_state_value
-
 
 
 
@@ -333,8 +407,16 @@ signal.signal(signal.SIGINT, signal_handler)
 print('Press Ctrl+C to stop')
 
 
-# run vis on main thread always...yes, much better
-vis_network_thread(global_network)
+try:
+    # run vis on main thread always...yes, much better
+    vis_network_thread(global_network)
+except Exception as e:
+    print "exception in vis thread, exiting"
+    stop_requested = True
+    print e
+
+
+
 
 # signal.pause() # vis thread effectively pauses!
 
@@ -355,10 +437,5 @@ print("*************************************************************************
 print('Now saving data. Please wait')
 
 
-if not os.path.exists(CHECKPOINT_DIR):
-  os.mkdir(CHECKPOINT_DIR)  
 
-saver.save(sess, CHECKPOINT_DIR + '/' + 'checkpoint', global_step = global_t)
-
-
-
+save_checkpoint(checkpoint_name)

a3c_training_thread.py
@@ -141,6 +141,9 @@ class A3CTrainingThread(object):
         # t_max times loop
         for i in range(LOCAL_T_MAX):
 
+            states.append(self.game_state.s_t)
+            lstm_states.append(lstm_state)
+
             pi_, value_, lstm_state = self.local_network.run(sess, self.game_state.s_t,
                                                              lstm_state)
 
@@ -151,11 +154,9 @@ class A3CTrainingThread(object):
             # pi_ = self.local_network.run_policy(sess, self.game_state.s_t)
             # action = choose_action(pi_)  # self.choose_action(pi_)
 
-            states.append(self.game_state.s_t)
             actions.append(action)
             # value_ = self.local_network.run_value(sess, self.game_state.s_t)
             values.append(value_)
-            lstm_states.append(lstm_state)
 
             if (self.thread_index == 0) and (self.local_t % 100) == 0:
                 print "pi=", pi_
@@ -204,15 +205,53 @@ class A3CTrainingThread(object):
         # self.local_network.lstm_last_output_state_value = lstm_state # preserve for next time through the loop
         self.lstm_last_output_state = lstm_state
 
+        #  TODO: cant store the lists i pass directly since they'll be destructively reversed by
+        # this call....hmmmmm
+        # maybe just reverse them here and leave it?
+        #  start with copying the lists
+        self.backup_and_accum_gradients(sess, global_t, summary_writer,
+                                        states=states,
+                                        lstm_states=lstm_states,
+                                        actions=actions,
+                                        values=values,
+                                        rewards=rewards,
+                                        final_reward_estimate=R)
+
+
+        if (self.thread_index == 0) and (self.local_t % 100) == 0:
+            print("TIMESTEP %d GLOBAL %d" % (self.local_t, global_t))
+
+        # 進んだlocal step数を返す
+        diff_local_t = self.local_t - start_local_t
+        return diff_local_t
+
+    def backup_and_accum_gradients(self, sess, global_t, summary_writer,
+                                   states, lstm_states, actions, values, rewards,
+                                   final_reward_estimate):
+        """ inputs are lists reflecting a recorded episode fragment in the order they occured
+
+            a = sample{ pi(a | s, lstm_s ) }
+            v = V(s, lstms)
+            r = env.step(a)
+
+        :param states: states
+        :param actions:
+        :param rewards:
+        :param lstm_states:
+        :return:
+        """
+
+
+        # TODO: copy these and leave the originals alone...
         actions.reverse()
         states.reverse()
         rewards.reverse()
         values.reverse()
         lstm_states.reverse()
 
+        R = final_reward_estimate
+
         # compute and accmulate gradients
-
-
         for (ai, ri, si, Vi, lstm_si) in zip(actions, rewards, states, values, lstm_states):
             R = ri + GAMMA * R
             td = R - Vi
@@ -235,6 +274,17 @@ class A3CTrainingThread(object):
             if (self.thread_index == 0):
                 summary_writer.add_summary(loss_summary, global_step=global_t)
 
+
+        """ idea: maybe possible to do n-step TBPTT after having retroactively computed R for each state
+        feed in batches of size up to n_max to a set of parallel networks with 
+        
+        
+        idea: set up the lstm with say 5 recursive calls. then the initial inputs would need to be padded...maybe?
+        would work if made the inputs in batches and altered iteration logic to cycle inputs through the history...
+        
+
+        """
+
         cur_learning_rate = self._anneal_learning_rate(global_t)
 
         _, grad_summary = sess.run([self.apply_gradients, self.grad_summary_op],
@@ -243,11 +293,118 @@ class A3CTrainingThread(object):
         if (self.thread_index == 0):
             summary_writer.add_summary(grad_summary, global_step=global_t)
 
-        if (self.thread_index == 0) and (self.local_t % 100) == 0:
-            print("TIMESTEP %d GLOBAL %d" % (self.local_t, global_t))
 
-        # 進んだlocal step数を返す
-        diff_local_t = self.local_t - start_local_t
-        return diff_local_t
+    #  TODO: rename 'states' variable as 'observations' in next version just to be fucking crystal clear
 
 
+    def process_memory(self, sess, global_t, summary_writer,
+                       states, initial_lstm_state, actions, rewards, final_state):
+        """
+        :param sess:
+        :param global_t:
+        :param summary_writer:
+        :param states:
+        :param initial_lstm_state:
+        :param actions:
+        :param rewards:
+        :param final_state:         observation after the last game step...use None to signal terminal, otherwise used
+         to compute the final boostrap Value
+        :return:
+        """
+
+        # TODO: gotcha initial_lstm_state must be set carefully
+        # if the episode reflects t=0, the state is always known
+        # otherwise how can we know what the lstm state output of the *current* policy might plausibly have been
+        # unless the same policy was executed from the very beginning of the historical episode and propagated
+        # we could just record the lstm_state prior to the beginning of the history episode as an approximation
+        # we might expect it to converge reasonably after a number of steps to something from the plausible distribution
+        # for the current policy...however, over time, the policy will drift away further and further from what
+        # created the original lstm_state
+        # this suggests the solution that we update the stored initial lstm state in the replay memory after every refresh
+        # ...almost like a real memory trace in a human brain might...
+        # but how can we update it ????
+        # maybe keep one state in reserve just to prime...but then we can only update the lstm state after it, not the one
+        # that initial state needs....HMMM. maybe just
+        #
+        # for certain environments we could just apply the network to s_t+0 repeatedly until the lstm state converges
+        # this works if the problem and/or env dont depend on any direct measure of time..perhaps
+        #
+        # easiest solution might just be to always reference the episodes to t=0
+        # or just ignore first k states when backing up and computing gradients...since presumably we'll have converged
+        # to something reasonable by that point
+
+
+
+        values = []
+        lstm_states = []
+
+        terminal_end = False
+
+        # reset accumulated gradients
+        sess.run(self.reset_gradients)
+
+        # copy weights from shared to local
+        sess.run(self.sync)
+        lstm_state = initial_lstm_state
+
+        for (s_t, a_t, r_t) in zip(states, actions, rewards):
+
+            # accum lstm states
+            lstm_states.append(lstm_state)
+
+            pi_, value_, lstm_state = self.local_network.run(sess, s_t, lstm_state)
+
+            # get values
+            values.append(values)
+
+
+
+        # what to do if terminal?
+        # set R = 0
+        # if not, recompute final_reward_estimate using value net and final
+
+
+# overkill for now but may be useful to tag with other info later
+class ReplayEpisode(object):
+
+    def __init__(self,
+                 observations,
+                 initial_lstm_state,
+                 actions,
+                 rewards,
+                 final_state
+                 ):
+        self.observations = observations
+        self.initial_lstm_state = initial_lstm_state
+        self.action = actions
+        self.rewards = rewards
+        self.final_state = final_state
+
+        self._total_reward = np.sum(rewards)
+
+    @property
+    def total_reward(self):
+        return self._total_reward
+
+import sortedcontainers
+
+class ReplayMemory(object):
+
+    def __init__(self, max_episodes):
+
+        self._max_episodes = max_episodes
+        # need a sorted map...by reward
+
+        self.episodes = sortedcontainers.SortedListWithKey(key=lambda x: x.total_reward)
+
+
+
+
+    def submit_episode(self, episode):
+
+        # check ep total reward and insert it in the map, discarding out of the middle on overflow
+
+
+        if len(self.episodes) > self._max_episodes:
+            # remove from random position in the middle 25% of the list
+            self.episodes.remove

gym_game_state.py
@@ -53,8 +53,8 @@ class GymGameState(object):
 
         self.reward = 0
         self.terminal = False
-        # self.s_t = np.stack((x_t, x_t, x_t, x_t), axis=1)
-        self.s_t = np.stack((x_t,), axis=1)
+        self.s_t = np.stack((x_t, x_t, x_t, x_t), axis=1)
+        # self.s_t = np.stack((x_t,), axis=1)
 
     def constrain_action(self, action):
         if isinstance(self.env.action_space, spaces.Box):

lowdim_ac_network.py
@@ -10,6 +10,27 @@ import random
 
 import uuid
 
+
+def _is_iterable(x):
+    return hasattr(x, "__iter__") \
+           and not isinstance(x, basestring) \
+           and not isinstance(x, tf.Variable)
+
+def flatten(x):
+    """ flattens a nested structure.
+        mind-bogglingly omitted from python.
+        what a joke
+    """
+    result = []
+
+    for el in x:
+        # if hasattr(el, "__iter__") and not isinstance(el, basestring):
+        if _is_iterable(el):
+            result.extend(flatten(el))
+        else:
+            result.append(el)
+    return result
+
 # global unique_string_counter
 # unique_string_counter = 0
 def unique_string():
@@ -26,6 +47,20 @@ def safe_log(x, arg_min=1e-10, arg_max=1):
 
 
 
+def get_basic_lstm_vars_from_scope(scope):
+    # print scope
+    # print scope.name
+    # vars = tf.get_collection(tf.GraphKeys.VARIABLES)
+    # print "all graph vars:", map(lambda x: x.name, vars)
+
+    lstm_variables = tf.get_collection(tf.GraphKeys.VARIABLES, scope=scope.name)
+
+    print "lstm_variables:"
+    print map(lambda x: x.name, lstm_variables)
+    Ws = filter(lambda x: "Matrix" in x.name, lstm_variables)
+    Bs = filter(lambda x: "Bias" in x.name, lstm_variables)
+    return Ws, Bs
+
 def get_lstm_vars_from_scope(scope):
     # print scope
     # print scope.name
@@ -33,11 +68,14 @@ def get_lstm_vars_from_scope(scope):
     # print "all graph vars:", map(lambda x: x.name, vars)
 
     lstm_variables = tf.get_collection(tf.GraphKeys.VARIABLES, scope=scope.name)
-    # print lstm_variables
+
+    # print "lstm_variables:"
     # print map(lambda x: x.name, lstm_variables)
-    Ws = filter(lambda x: "Matrix" in x.name, lstm_variables)
-    Bs = filter(lambda x: "Bias" in x.name, lstm_variables)
+    Ws = filter(lambda x: "LSTMCell/W" in x.name, lstm_variables)
+    Bs = filter(lambda x: "LSTMCell/B" in x.name, lstm_variables)
     return Ws, Bs
+
+
 
 # Actor-Critic Network (Policy network and Value network)
 
@@ -172,7 +210,7 @@ class LowDimACNetwork(object):
         batch_size = 1
         with tf.device(self._device), tf.name_scope(self.network_name):
 
-            input_history = 1
+            input_history = 4
 
             # input
             self.s = tf.placeholder("float", [1, self._input_size, input_history]) #[1, 84, 84, 4])
@@ -202,12 +240,18 @@ class LowDimACNetwork(object):
                     layer_name = self.network_name + "/" + name + "/lstm0"
                     with tf.variable_scope(layer_name) as vs:
                         # just 1 for now
-                        lstm = tf.nn.rnn_cell.BasicLSTMCell(self._lstm_sizes[0])
+                        # lstm = tf.nn.rnn_cell.BasicLSTMCell(self._lstm_sizes[0])
+                        lstm = tf.nn.rnn_cell.LSTMCell(self._lstm_sizes[0], use_peepholes=True) # try fancier lstm .... grr returns more weight matrices fuck
                         # todo: append stats somehow
 
                         current_state_tensor = tf.placeholder("float", [batch_size, lstm.state_size])
                         self.lstm_current_state_tensors.append(current_state_tensor)
                                                                 #tf.zeros([batch_size, lstm.state_size])
+
+                        # todo: use # of steps in second index on input
+                        #  reshape the input tensor to fit better in the computations
+                        #             tf.get_variable_scope().reuse_variables()
+
 
                         y, output_state_tensor = lstm(x, current_state_tensor)
                         self.lstm_output_state_tensors.append(output_state_tensor)
@@ -216,16 +260,26 @@ class LowDimACNetwork(object):
                         self._lstm_initial_state_value.append(np.zeros(dtype="float32",shape=[batch_size, lstm.state_size ]))
 
                         # retrieve variables
-                        Ws, Bs = get_lstm_vars_from_scope(vs)
+                        Ws, bs = get_lstm_vars_from_scope(vs)
                         #  we arent equipped to deal with more complex setups yet
                         # print "lstm Ws:", Ws
-                        assert len(Ws) == 1
-                        assert len(Bs) == 1
+                        # assert len(Ws) == 1
+                        # assert len(bs) == 1
                         # manually add variable summaries
-                        self.variable_summaries(Ws[0], "W_%s" % layer_name)
-                        self.variable_summaries(Bs[0], "b_%s" % layer_name)
+                        print "Ws:", Ws
+                        print "bs:", bs
 
-                        layers.append((Ws[0],Bs[0],y,layer_name))
+                        for W in Ws:
+                            print "Adding summaries for layer %s : %s=%s" % (layer_name, W.name, W)
+                            self.variable_summaries(W, "W_%s_%s" % (layer_name, W.name))
+
+                        for b in bs:
+                            print "Adding summaries for layer %s : %s=%s" % (layer_name, b.name, b)
+                            self.variable_summaries(b, "b_%s_%s" % (layer_name, b.name))
+
+
+                        layers.append((Ws,bs,y,layer_name))     # can now add tuples of weights or biases to this list
+
                         x = y
                         input_size = self._lstm_sizes[0]
 
@@ -240,6 +294,7 @@ class LowDimACNetwork(object):
 
                 # hidden_sigma2_output, hidden_sigma2_output_size = hidden_layers(input_state_flat, input_size, name="sigma2_net")
                 W_sigma2, b_sigma2, sigma2 = self._fc_layer(hidden_policy_output, hidden_policy_output_size, 1, activation=tf.nn.softplus, name="sigma2")
+                sigma2 = tf.maximum(sigma2, 1e-3)  # MINOR HACK: bound variance so that our log probs dont get fucked
                 layers.append((W_sigma2, b_sigma2, sigma2, "sigma2"))
 
                 self.mu = mu
@@ -308,6 +363,8 @@ class LowDimACNetwork(object):
             l.extend(summaries)
             l += [tf.scalar_summary(["R"], self.r)]
             l += [tf.scalar_summary(["(R-V)"], self.td)]
+            l += [tf.scalar_summary(["V (loss eval)"], tf.reshape(self.v, (1,)))]
+            l += [tf.scalar_summary(["V (r-td)"], self.r - self.td)]
             l += [tf.scalar_summary(["entropy"], tf.reshape(entropy, (1,)))]
             l += [tf.scalar_summary(["policy_loss"], tf.reshape(policy_loss, (1,)))]    # TODO: HACK: when we do batch mode, will want a histogram and ditch the reshape, most likely?
             l += [tf.scalar_summary("value_loss", value_loss)]
@@ -341,8 +398,23 @@ class LowDimACNetwork(object):
 
         return \
             -0.5*n*safe_log(var,arg_min=1e-6, arg_max=float("inf")) \
-            + (-0.5 * (1/tf.maximum(var_to_the_n, 1e-6)) * tf.reduce_sum(tf.squared_difference(x, mean), reduction_indices=(1,)))
+            + (-0.5 * (1/tf.maximum(var_to_the_n, 1e-24)) * tf.reduce_sum(tf.squared_difference(x, mean), reduction_indices=(1,)))
 
+
+    def _tf_inspired_mvn_log_pdf(self, x, mean, var):
+
+        k = tf.cast(tf.size(mean), tf.float32)
+
+        log_two_pi = tf.constant(math.log(2 * math.pi), dtype=tf.float32)
+        x_whitened = (x - mean) / tf.sqrt(var)
+        x_whitened_norm = tf.reduce_sum(x_whitened * x_whitened)
+
+        sigma_det = tf.pow(var, k)
+
+        log_pdf_value = (
+          -tf.log(sigma_det) - k * log_two_pi - x_whitened_norm) / 2
+
+        return log_pdf_value
 
 
     def _prepare_policy_loss_continuous(self, entropy_beta):
@@ -359,20 +431,48 @@ class LowDimACNetwork(object):
 
 
         # policy probablity pi(a | s_t, theta)
-        sampled_action_probability = self._multivariate_normal_pdf(self.a, self.mu, self.sigma2)
-        sampled_action_log_probability = self._multivariate_normal_log_pdf(self.a, self.mu, self.sigma2)
 
-        summaries += [tf.scalar_summary(["log P(a) log of pdf"],
-                                        tf.reshape(safe_log(sampled_action_probability, arg_min=1e-30), (1,)))]
-        summaries += [tf.scalar_summary(["log P(a) log prob"],
+        cov = self.sigma2 * tf.constant(np.identity(self._action_size), dtype=tf.float32)
+        cov = tf.reshape(cov, shape=(1,self._action_size, self._action_size)) # mvn needs batch index to stay
+        print "Sigma2 shape,", self.sigma2.get_shape()
+        print "cov shape,", cov.get_shape()
+        print "mu shape,", self.mu.get_shape()
+        mvn = tf.contrib.distributions.MultivariateNormal(self.mu, sigma_chol=tf.sqrt(cov))
+
+        sampled_action_probability = self._multivariate_normal_pdf(self.a, self.mu, self.sigma2)
+        # sampled_action_log_probability_homebrew = self._multivariate_normal_log_pdf(self.a, self.mu, self.sigma2)
+        sampled_action_log_probability_homebrew = self._tf_inspired_mvn_log_pdf(self.a, self.mu, self.sigma2)
+        sampled_action_log_probability = tf.maximum(mvn.log_pdf(self.a), -1e+10)
+
+        # summaries += [tf.scalar_summary(["log P(a) log of pdf"],
+        #                                 tf.reshape(safe_log(sampled_action_probability, arg_min=1e-30), (1,)))]
+        summaries += [tf.scalar_summary(["log P(a) tf"],
                                         tf.reshape(sampled_action_log_probability, (1,)))]
-        summaries += [tf.scalar_summary(["log P(a) (log prob - lof of pdf)"],
-                                        tf.reshape(sampled_action_log_probability - safe_log(sampled_action_probability, arg_min=1e-30), (1,)))]
+        summaries += [tf.scalar_summary(["log P(a) homebrew"],
+                                        tf.reshape(sampled_action_log_probability_homebrew, (1,)))]
+        # summaries += [tf.scalar_summary(["log P(a) (log prob - lof of pdf)"],
+        #                                 tf.reshape(sampled_action_log_probability - safe_log(sampled_action_probability, arg_min=1e-30), (1,)))]
+
+        # summaries += [tf.scalar_summary(["log P(a) (log prob - lof of pdf)"],
+        #                                 tf.reshape(sampled_action_log_probability - safe_log(sampled_action_probability, arg_min=1e-30), (1,)))]
+
+        summaries += [tf.scalar_summary(["logprob_homebrew(a) - logprob_tf(a)"],
+                                        tf.reshape(sampled_action_log_probability_homebrew - sampled_action_log_probability, (1,)))]
+
+
+
 
 
         summaries += [tf.scalar_summary(["sigma2"], tf.reshape(self.sigma2, (1,)))]
 
         summaries += [tf.histogram_summary("mu", self.mu )]
+
+        """ NEXT STEP:
+        - determined that tf's mvn logprob gives right answers whereas mine doesnt for dimns > 1, but agrees with dim == 1
+        - deterimend that tf's mvn logprob FUCKS the gradient, whereas mine doesnt
+        - find the error in my logprob
+        - may be purely numerical...could help to whiten x before reducing
+         """
 
 
 
@@ -380,7 +480,7 @@ class LowDimACNetwork(object):
         # TODO: HACK: reduce_sum here even though
 
         # policy_loss = safe_log(sampled_action_probability) * (self.td + entropy * entropy_beta)
-        policy_loss = sampled_action_log_probability * (self.td + entropy * entropy_beta)
+        policy_loss = sampled_action_log_probability_homebrew * (self.td + entropy * entropy_beta)
 
         #  TODO: cleanup non-log policy
 
@@ -524,8 +624,9 @@ class LowDimACNetwork(object):
 
         vars = []
         for (W,b,y,name) in self._layers:
-            vars.append(W)
-            vars.append(b)
+            # vars.append(W)
+            vars.extend(flatten([W]))
+            vars.extend(flatten([b]))
 
         return vars
         # return [
@@ -562,15 +663,15 @@ class LowDimACNetwork(object):
 
     def _fc_weight_variable(self, shape, name="anon_weight"):
         input_channels = shape[0]
-        # d = 1.0 / np.sqrt(input_channels)
-        d = 1.0 / input_channels**2
+        d = 1.0 / np.sqrt(input_channels)
+        # d = 1.0 / input_channels**2
         initial = tf.random_uniform(shape, minval=-d, maxval=d)
         return tf.Variable(initial, name=name)
 
 
     def _fc_bias_variable(self, shape, input_channels, name="anon_bias"):
-        # d = 1.0 / np.sqrt(input_channels)
-        d = 1.0 / input_channels**2
+        d = 1.0 / np.sqrt(input_channels)
+        # d = 1.0 / input_channels**2
         initial = tf.random_uniform(shape, minval=-d, maxval=d)
         return tf.Variable(initial, name=name)
 
@@ -606,7 +707,11 @@ class LowDimACNetwork(object):
         # self._save_sub(sess, prefix, self.W_conv2, "W_conv2")
         # self._save_sub(sess, prefix, self.b_conv2, "b_conv2")
 
+
+
         for (W,b,y,name) in self._layers:
+            # TODO: not implemented yet for multi-weight layers
+            assert(len(W) == 1)
             self._save_sub(sess, prefix, W, W.name)
             self._save_sub(sess, prefix, b, b.name)
 